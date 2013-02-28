e1e399c4953ea12aa2d2eb2c88460720c184efef
Dwayne Charrington <dwayencharrington@gmail.com>
2013-02-28 12:44:23 +1000

    Added in support for multiple colours using percentage values. Updated the README to reflect new changes.

--- jquery.gradientizer.js
+++ jquery.gradientizer.js
@@ -14,8 +14,21 @@
  * Simple usage:
  *
  * $("#somediv").gradientizer({
- *     from: '#000000',
- *     to: '#FFFFFF'  
+ *     direction: 'horizontal',
+ *     colors: [
+ *         {
+ *           '0%': 'red',
+ *           '10%': 'yellow',
+ *           '20%': 'pink',
+ *           '30%': 'green',
+ *           '40%': 'purple',
+ *           '50%': 'orange',
+ *           '70%': 'blue',
+ *           '80%': 'magenta',
+ *           '90%': 'cyan',
+ *           '100%': 'indigo'
+ *         }
+ *     ]
  * });
  *
  */
@@ -23,34 +36,72 @@
 
     $.fn.gradientizer = function(options) {
 
-        options = $.extend({ from: '#000000', to: '#ffffff', direction: 'vertical'}, options || {});
+        var defaults = {
+            direction: 'vertical',
+            colors: [
+                {
+                    '0%': 'red',
+                    '10%': 'yellow',
+                    '20%': 'pink',
+                    '30%': 'green',
+                    '40%': 'purple',
+                    '50%': 'orange',
+                    '70%': 'blue',
+                    '80%': 'magenta',
+                    '90%': 'cyan',
+                    '100%': 'indigo'
+                }
+            ]
+        };
+
+        // Merge defaults and options into the one settings object
+        options = $.extend({}, defaults, options); 
 
         return this.each(function() {
             var $this = $(this);
 
+            console.log(createCssString());
+
             switch (options.direction) {
 
                 case 'vertical':
-                    // You would think this would merely overwrite but it actually works
-                    $this.css('background', 'linear-gradient(top, '+options.from+' 0%, '+options.to+' 100%)');
-                    $this.css('background', '-webkit-linear-gradient(top, '+options.from+' 0%, '+options.to+' 100%)');
-                    $this.css('background', '-o-linear-gradient(top, '+options.from+' 0%, '+options.to+' 100%)');
-                    $this.css('background', '-moz-linear-gradient(top, '+options.from+' 0%, '+options.to+' 100%)');
-                    $this.css('background', '-ms-linear-gradient(top, '+options.from+' 0%, '+options.to+' 100%)');
+                    // Default background colour for browsers that don't support shit
+                    $this.css('background', ''+options.colors[0]['0%']+'');
+
+                    $this.css('background', 'linear-gradient(top, '+createCssString()+')');
+                    $this.css('background', '-webkit-linear-gradient(top, '+createCssString()+')');
+                    $this.css('background', '-o-linear-gradient(top, '+createCssString()+')');
+                    $this.css('background', '-moz-linear-gradient(top, '+createCssString()+')');
+                    $this.css('background', '-ms-linear-gradient(top, '+createCssString()+')');
                 break;
 
                 case 'horizontal':
-                    // You would think this would merely overwrite but it actually works
-                    $this.css('background', 'linear-gradient(right top, '+options.from+' 0%, '+options.to+' 100%)');
-                    $this.css('background', '-webkit-linear-gradient(right top, '+options.from+' 0%, '+options.to+' 100%)');
-                    $this.css('background', '-o-linear-gradient(right top, '+options.from+' 0%, '+options.to+' 100%)');
-                    $this.css('background', '-moz-linear-gradient(right top, '+options.from+' 0%, '+options.to+' 100%)');
-                    $this.css('background', '-ms-linear-gradient(right top, '+options.from+' 0%, '+options.to+' 100%)');
+                    // Default background colour for browsers that don't support shit
+                    $this.css('background', ''+options.colors[0]['0%']+'');
+
+                    $this.css('background', 'linear-gradient(right top, '+createCssString()+')');
+                    $this.css('background', '-webkit-linear-gradient(right top, '+createCssString()+')');
+                    $this.css('background', '-o-linear-gradient(right top, '+createCssString()+')');
+                    $this.css('background', '-moz-linear-gradient(right top, '+createCssString()+')');
+                    $this.css('background', '-ms-linear-gradient(right top, '+createCssString()+')');
                 break;
 
             }
 
         });
+
+        function createCssString() {
+            var finalOutput = '';
+
+            $.each(options.colors[0], function(key, value) {
+                finalOutput += ""+value+" "+key+", ";
+            });
+
+            finalOutput = $.trim(finalOutput);
+
+            return finalOutput.replace(/,+$/,'');
+        }
+
     };
 
 })(jQuery);
--- readme.md
+++ readme.md
@@ -2,28 +2,45 @@
 ## Gradientizer
 ### CSS3 gradients for jQuery
 
-For some reason jQuery out of the box doesn't make it easy to add gradients to elements. Sure you can add in a whole bunch of CSS style overrides for each browser, but who has time to do that? Enter Gradientizer. It might not be the prettiest or most moral plugins of them all out there, but it works. Essentially all this plugin does is allows you to specify a starting and ending colour value as well as gradient direction (vertical or horizontal).
+For some reason jQuery out of the box doesn't make it easy to add gradients to elements. Sure you can add in a whole bunch of CSS style overrides for each browser, but who has time to do that? Enter Gradientizer. It might not be the prettiest or most moral plugins of them all out there, but it works. Essentially this colour allows you to add cross-browser gradients to your sites, with support for multiple colours (as shown in our example) as well as gradient direction (vertical or horizontal).
 
-There are future plans such as feature detection and support for more than 2 gradient colours such as being able to specify a third colour showing at 75% of the gradient. You can easily code this in yourself if you need it, but at present this plugin doesn't support it.
+There are future plans for feature detection, but the plugin works great as it stands.
 
-The Gradientizer plugin relies on CSS3 gradients, so not all browsers support these and will fallback to a flat colour if not supported. There is no support for browsers that do not support CSS3 gradients and never will be, so if this is a concern then you are in the wrong neighbourhood boy and this plugin isn't fo' you.
+The Gradientizer plugin relies on CSS3 gradients, so not all browsers support these and will fallback to a flat colour if not supported. There is no support for browsers that do not support CSS3 gradients and never will be, so if this is a concern then you are in the wrong neighbourhood boy and this plugin isn't fo' you. To see what browsers support CSS3 gradients, check <a href="http://caniuse.com/css-gradients">this helpful link</a> out.
 
 ## Example Usage
 
 Ensure you have included jQuery first before using Gradientizer.
 
-(function($) {
+<pre>
+	<code>
+	(function($) {
 
-	// The new cool way to write document.ready
-	$(function() {
+		// The new cool way to write document.ready
+		$(function() {
 
-		$("#somediv").gradientizer({
-			from: '#000000',
-			to: '#FFFFFF'  
+			$("#somediv").gradientizer({
+				direction: 'horizontal',
+				colors: [
+				  {
+				    '0%': 'red',
+				    '10%': 'yellow',
+				    '20%': 'pink',
+				    '30%': 'green',
+				    '40%': 'purple',
+				    '50%': 'orange',
+				    '70%': 'blue',
+				    '80%': 'magenta',
+				    '90%': 'cyan',
+				    '100%': 'indigo'
+				  }
+				]
+			});
+
 		});
-
-	});
-})(jQuery);
+	})(jQuery);
+	</code>
+</pre>
 
 ## Contribute
 
